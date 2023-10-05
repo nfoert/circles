e671be6c9000330ca189e424215c6b451399b5df
nfoert <nofoert@gmail.com>
2023-10-04 21:32:09 -0400

    Only switch Circles if you're not already doing so Cuts down on bugginess but there's still some small problems when spam clicking

--- circles/main/static/main/scripts/location.js
+++ circles/main/static/main/scripts/location.js
@@ -10,6 +10,8 @@ var location_box = document.getElementById("main-location-box-location")
 var location_box_users = document.getElementById("main-location-box-users")
 var location_box_location = document.getElementById("main-location-box-location")
 
+var changing_circle = false;
+
 
 function set_event_listeners() {
     // Set event listeners to each clickable item in the box
@@ -42,29 +44,37 @@ function update_position_indicator() {
 }
 
 function location_change_circle(event) {
-    location_items = location_box_location.getElementsByClassName("location-item");
-    let position = [];
+    if (changing_circle == false) {
+        changing_circle = true;
 
-    for (item in location_items) {
-        if (location_items[item].innerText != event.target.innerText) {
-            position.push(location_items[item].innerText)
-        
-        } else {
-            position.push(location_items[item].innerText)
-            break;
+        location_items = location_box_location.getElementsByClassName("location-item");
+        let position = [];
+
+        for (item in location_items) {
+            if (location_items[item].innerText != event.target.innerText) {
+                position.push(location_items[item].innerText)
+            
+            } else {
+                position.push(location_items[item].innerText)
+                break;
+            }
         }
-    }
-    
-    let change_circle_json = {
-        "type": "change_circle",
-        "direction": "absolute",
-        "name": position,
-    }
+        
+        let change_circle_json = {
+            "type": "change_circle",
+            "direction": "absolute",
+            "name": position,
+        }
 
-    server_socket.send(JSON.stringify(change_circle_json))
+        server_socket.send(JSON.stringify(change_circle_json))
 
-    me.location_circle = location; // TODO: What if you switch to the server?
-    update_position_indicator();
+        me.location_circle = location; // TODO: What if you switch to the server?
+        setTimeout(function() {
+            update_position_indicator();
+            changing_circle = false;
+        }, 500)
+
+    }
 }
 
 function update_user_count(json) {

8f31e1973a0bbc42da00b54b78f21f7bfeaf5907
nfoert <nofoert@gmail.com>
2023-10-30 11:37:36 -0400

    Add STATICFILES_DIRS setting Adjust virtual enviroment location in tasks to match repo instructions

--- circles/circles/production_settings.py
+++ circles/circles/production_settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'main/static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

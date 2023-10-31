9b25dcf5df285ba00576514ac09edca06fc2077e
nfoert <nofoert@gmail.com>
2023-10-31 11:36:14 -0400

    Rearrange location for static files

--- circles/circles/development_settings.py
+++ circles/circles/development_settings.py
@@ -133,9 +133,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Why do I do this to myself -_-
 STATICFILES_DIRS = (
-    Path(BASE_DIR).parent / Path("main/static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type

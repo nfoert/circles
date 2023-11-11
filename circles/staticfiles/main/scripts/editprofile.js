0a20a952e2942d61b7626b20f07f84a2debc0497
nfoert <nofoert@gmail.com>
2023-11-11 11:54:02 -0500

    - Static files

--- circles/staticfiles/main/scripts/editprofile.js
+++ circles/staticfiles/main/scripts/editprofile.js
@@ -40,6 +40,16 @@ edit_profile_signout.addEventListener("click", sign_out);
 edit_profile_changepassword.addEventListener("click", not_implemented);
 edit_profile_deleteaccount.addEventListener("click", not_implemented);
 
+var settings = document.getElementsByClassName("setting")
+
+for (const setting in settings) {
+
+    if (settings[setting].nodeName == "INPUT") {
+        settings[setting].addEventListener("change", update_setting)
+
+    }
+}
+
 function not_implemented() {
     show_notification("<i class='ph-bold ph-warning'></i> Not Implemented", "That feature has not been implemented yet. Check the Issues page for more information at github.com/nfoert/circles/issues", "normal", true);
     set_notification_color(255, 0, 0);
@@ -66,6 +76,20 @@ function render_profile_details(json) {
     edit_profile_primary.value = json["primary_color"];
     edit_profile_secondary.value = json["secondary_color"];
 
+    var settings = document.getElementsByClassName("setting")
+
+    for (const setting in settings) {
+        if (settings[setting].nodeName == "INPUT") {
+            if (settings[setting].getAttribute("setting") in json["settings"]) {
+                settings[setting].checked = json["settings"][settings[setting].getAttribute("setting")];
+            }
+
+            else {
+                settings[setting].checked = settings[setting].getAttribute("default"); 
+            }
+        }
+    }
+
     open_edit_profile();
 }
 
@@ -78,12 +102,23 @@ function render_profile_details_from_fields() {
 }
 
 function save_profile_details() {
+    var settings = document.getElementsByClassName("setting")
+    let settings_json = {}
+
+    for (const setting in settings) {
+        if (settings[setting].nodeName == "INPUT") {
+            settings_json[settings[setting].getAttribute("setting")] = settings[setting].checked;
+        }
+    }
+
+
     let save_profile_details_request = {
         "type": "save_profile_details",
         "display_name": edit_profile_displayname.value,
         "bio": edit_profile_bio.value,
         "primary_color": edit_profile_primary.value,
-        "secondary_color": edit_profile_secondary.value
+        "secondary_color": edit_profile_secondary.value,
+        "settings": settings_json
     }
 
     server_socket.send(JSON.stringify(save_profile_details_request))
@@ -133,4 +168,8 @@ function close_edit_profile() {
     setTimeout(function() {
         edit_profile.style.display = "none";
     }, 500);
+}
+
+function update_setting(e) {
+    console.log(e.target)
 }
